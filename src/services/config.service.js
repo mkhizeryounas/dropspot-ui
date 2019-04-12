3b731017b0e7f6be136cbde1a484ab3dc3a2eeff
Khizer Younas <m.khizeryounas@gmail.com>
2019-04-13 04:15:14 +0500

    edited isauth

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -1,2 +1,2 @@
-export const base_url = "http://localhost:3000";
-// export const base_url = "http://165.227.238.65:8080";
+// export const base_url = "http://localhost:3000";
+export const base_url = "http://165.227.238.65:8080";
